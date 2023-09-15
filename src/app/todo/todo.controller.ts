b7749bc38e8b7ef8f79123bdfa232d211dcbd511
Henrique Damasceno <henriquim25@yahoo.com.br>
2023-09-15 01:08:43 -0300

    feat: implemeted the crud todo

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { TodoService } from './todo.service';
 
-@Controller('ap1/v1/todos')
-export class TodoController {}
+@Controller('todos')
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Post()
+  async create(@Body() body) {
+    return this.todoService.create(body);
+  }
+
+  @Get()
+  async findAll() {
+    return this.todoService.findAll();
+  }
+
+  @Get(':id')
+  async findById(@Param('id', ParseUUIDPipe) id: string) {
+    return this.todoService.findOne(id);
+  }
+
+  @Patch(':id')
+  async update(@Param('id', ParseUUIDPipe) id: string, @Body() body) {
+    return this.todoService.update(id, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async delete(@Param('id', ParseUUIDPipe) id: string) {
+    await this.todoService.deleteById(id);
+  }
+}
--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return this.todoRepository.find({
+      withDeleted: true,
+    });
+  }
+
+  async findOne(id: string) {
+    const entity = await this.todoRepository.findOne({
+      where: {
+        id: id,
+      },
+    });
+    if (!entity) {
+      throw new NotFoundException();
+    }
+    return entity;
+  }
+
+  async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async update(id: string, data) {
+    const todo = await this.findOne(id);
+
+    this.todoRepository.merge(todo, data);
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteById(id: string) {
+    await this.todoRepository.softDelete(id);
+  }
+}
