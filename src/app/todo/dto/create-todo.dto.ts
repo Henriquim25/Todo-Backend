6a1e6737ed1eff93ae58a4e7aad7fd3a693bf953
Henrique Damasceno <henriquim25@yahoo.com.br>
2023-09-15 02:26:11 -0300

    feat: documented the api

--- src/app/todo/dto/create-todo.dto.ts
+++ src/app/todo/dto/create-todo.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import { IsBoolean, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateTodoDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   task: string;
 
   @IsBoolean()
   @IsOptional()
   @Type(() => Boolean)
+  @ApiPropertyOptional()
   isDone: boolean = false;
 }
--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -13,27 +13,39 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('todos')
+@ApiTags('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Criar um Todo' })
+  @ApiResponse({ status: 201, description: 'Tarefa Criada' })
+  @ApiResponse({ status: 400, description: 'Parametros Invalidos' })
   async create(@Body() body: CreateTodoDto) {
     return this.todoService.create(body);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Listar todos Todo' })
+  @ApiResponse({ status: 200, description: 'Lista de Tarefas' })
   async findAll() {
     return this.todoService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Listar um Todo pelo ID' })
+  @ApiResponse({ status: 200, description: 'Tarefa selecionada pelo ID' })
   async findById(@Param('id', ParseUUIDPipe) id: string) {
     return this.todoService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Atualizar um Todo pelo ID' })
+  @ApiResponse({ status: 400, description: 'Parametros Invalidos' })
+  @ApiResponse({ status: 200, description: 'Tarefa Atualizada' })
   async update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() body: UpdateTodoDto,
@@ -43,6 +55,9 @@ export class TodoController {
 
   @Delete(':id')
   @HttpCode(HttpStatus.NO_CONTENT)
+  @ApiOperation({ summary: 'Deletar um Todo pelo ID' })
+  @ApiResponse({ status: 201, description: 'Deletado com Sucesso' })
+  @ApiResponse({ status: 400, description: 'Id invalido' })
   async delete(@Param('id', ParseUUIDPipe) id: string) {
     await this.todoService.deleteById(id);
   }
